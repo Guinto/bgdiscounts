3dc328e9826e9143fd397e1e0385875b25bb2b75
Trent Ellingsen <trentellingsen@gmail.com>
2019-05-11 13:03:22 -0700

    init

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    request({ url: "https://www.boardgameatlas.com/api/search?order_by=popularity&gt_discount=0.4&client_id=55QzrLZsXp" } , function(err, response, jsonString) {
+        var json = JSON.parse(jsonString);
+        var games = json.games
+        var options = {
+            title: 'Board Game Discounts',
+            games: games
+        }
+        res.render('index', options);
+    });
+});
+
+module.exports = router;
